133bfe4cca62d58426e2923ec95fb8671361b09d
zuoez02 <zuoez02@outlook.com>
2016-04-26 18:06:41 +0800

    First commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+// Remember to put this file in your src/ directory
+
+// import greeting from "./some_module";
+
+console.log('fuck');
+// console.log(greeting);
---
+++ src/some_module.ts
@@ -0,0 +1,5 @@
+// Remember to put this file in your src/ directory
+
+const greeting: string = "Hello World!";
+
+export default greeting;
